990d7701dfdab80e3cb440a7a339ab0762c6e2f5
alternoegraha <noegrahachan@gmail.com>
2024-04-29 07:38:53 +0000

    fog: EvoX bringup again :DDD
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_fog.mk
+    $(LOCAL_DIR)/evolution_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fog-ap1a-user \
-    aosp_fog-ap1a-userdebug \
-    aosp_fog-ap1a-eng
+    evolution_fog-user \
+    evolution_fog-userdebug \
+    evolution_fog-eng
--- evolution_fog.mk
+++ evolution_fog.mk
@@ -8,16 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some EvoX stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_PHONE_LINK_SUPPORTED := false
+TARGET_USES_MINI_GAPPS := true
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := aosp_fog
+PRODUCT_NAME := evolution_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
