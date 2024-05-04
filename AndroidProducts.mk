5f4923f45812c303e3c055c28085a1d70393a09e
arsha-oss <156425925+arsha-oss@users.noreply.github.com>
2024-05-04 18:25:01 +0700

    Change to arrowos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_fog.mk
+    $(LOCAL_DIR)/arrow_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_fog-ap1a-user \
-    aosp_fog-ap1a-userdebug \
-    aosp_fog-ap1a-eng
+    arrow_fog-user \
+    arrow_fog-userdebug \
+    arrow_fog-eng
